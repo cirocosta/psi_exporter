817f0122fc95a3b4e79dd84fb0e58df0c2f542a5
Ciro S. Costa <cscosta@pivotal.io>
2019-02-09 20:59:16 -0500

    Wire collector with parser
    Signed-off-by: Ciro S. Costa <cscosta@pivotal.io>

--- collector.go
+++ collector.go
@@ -1,47 +1,79 @@
 package main
 
 import (
+	"os"
+
+	"github.com/cirocosta/psi_exporter/parser"
+	"github.com/pkg/errors"
 	"github.com/prometheus/client_golang/prometheus"
 )
 
 type PsiCollector struct{}
 
 var (
-	_ prometheus.Collector = (*PsiCollector)(nil)
+	_          prometheus.Collector = (*PsiCollector)(nil)
+	subsystems                      = []string{"cpu", "io", "memory"}
+	metrics                         = []string{"avg10", "avg60", "avg300", "total"}
 )
 
 func init() {
 	prometheus.Register(&PsiCollector{})
 }
 
-// some avg10=0.00 avg60=0.00 avg300=0.00 total=3435136
 func (c *PsiCollector) Describe(ch chan<- *prometheus.Desc) {
-	ch <- prometheus.NewDesc(
-		"psi_cpu_some_avg10",
-		"todo",
-		nil,
-		nil)
-
-	ch <- prometheus.NewDesc(
-		"psi_cpu_some_avg60",
-		"todo",
-		nil,
-		nil)
-
-	ch <- prometheus.NewDesc(
-		"psi_cpu_some_avg300",
-		"todo",
-		nil,
-		nil)
-
-	ch <- prometheus.NewDesc(
-		"psi_cpu_some_total",
-		"todo",
-		nil,
-		nil)
 	return
 }
 
+func collectRowsFromFilepath(filepath string) (rows []*parser.Row, err error) {
+	file, err := os.Open(filepath)
+	if err != nil {
+		err = errors.Wrapf(err,
+			"failed to open file %s for parsing", filepath)
+		return
+	}
+	defer file.Close()
+
+	rows, err = parser.Parse(file)
+	if err != nil {
+		err = errors.Wrapf(err,
+			"failed to parse memory stats from %s",
+			file.Name())
+		return
+	}
+
+	return
+}
+
+func psiStatDescription(subsystem, metric string) *prometheus.Desc {
+	return prometheus.NewDesc(
+		"psi_"+subsystem+"_"+metric,
+		"Values for "+metric+" under /proc/pressure/"+subsystem,
+		[]string{"type"},
+		nil)
+}
+
 func (c *PsiCollector) Collect(ch chan<- prometheus.Metric) {
+	var structVals [4]*float64
+
+	for _, subsystem := range subsystems {
+		rows, err := collectRowsFromFilepath("/proc/pressure/" + subsystem)
+		if err != nil {
+			panic(err)
+		}
+
+		for _, row := range rows {
+			structVals = [4]*float64{&row.Avg10, &row.Avg60, &row.Avg300, &row.Total}
+
+			for idx, metric := range metrics {
+				ch <- prometheus.MustNewConstMetric(
+					psiStatDescription(subsystem, metric),
+					prometheus.UntypedValue,
+					*structVals[idx],
+					row.Type,
+				)
+			}
+		}
+	}
+
 	return
 }
